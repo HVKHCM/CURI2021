bb9e62a83c0822fd375d12a2256860d8e81a29de
Dick Brown <rab@stolaf.edu>
2021-06-11 15:27:25 -0500

    add mpi4py-cleanup.sh

--- test1.sh
+++ test1.sh
@@ -2,6 +2,9 @@
 # to execute:
 # git clone https://github.com/HVKHCM/CURI2021-Raspberry-Pi.git stest; stest/test1.sh
 
+cd ~
+./mpi4py-cleanup.sh
+
 cd /home/pi/stest
 
 ./patch.sh || { echo "patch.sh failed" ; exit 1; }
