4e086276e4ae0fd476bb8b12fb5d2fd9b24c3a7c
HVKHCM <49514004+HVKHCM@users.noreply.github.com>
2021-06-11 17:37:17 -0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -10,10 +10,8 @@ EOF
 
 # fix /usr/HD, add xhost +
 echo Issuing xhost +
-sudo -i
 cd /usr/HD
 sudo chmod 755 .
-sudo cp head-node.bash head-node.bash.bu
+sudo -i cp head-node.bash head-node.bash.bu
 sudo awk 'NR==123 {print "    xhost +"} {print}' head-node.bash.bu > head-node.bash
 sudo chmod +x head-node.bash
-exit
