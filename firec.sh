9ef5685480cdd218c27c567c9584f8376ba46a5f
Dick Brown <rab@stolaf.edu>
2021-06-12 08:08:27 -0500

    update firec.sh

--- firec.sh
+++ firec.sh
@@ -4,6 +4,7 @@
 # git clone https://github.com/HVKHCM/CURI2021-Raspberry-Pi.git ctest; ctest/firec.sh
 
 # test fire with cluster
+echo "Testing fire example with cluster"
 cd ~/CSinParallel/Exemplars/mpi4py-examples/fire/
 cp ~/ctest/fire_mpi_simulate.py .
 mpirun -np 4 -hostfile ~/hostfile python fire_mpi_simulate.py 10 0.1 20
