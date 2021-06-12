8feb704debe02fd8f449a29451892851aa82b1fc
Dick Brown <rab@stolaf.edu>
2021-06-12 08:00:14 -0500

    update firec.sh

--- firec.sh
+++ firec.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # test fire on cluster and cleanup after cluster-set test
 # to execute:
-# git clone https://github.com/HVKHCM/CURI2021-Raspberry-Pi.git stest; stest/firec.sh
+# git clone https://github.com/HVKHCM/CURI2021-Raspberry-Pi.git ctest; ctest/firec.sh
 
 # test fire with cluster
-#soc-mpisetup -w
 cd ~/CSinParallel/Exemplars/mpi4py-examples/fire/
+cp ~/ctest/fire_mpi_simulate.py .
 mpirun -np 4 -hostfile ~/hostfile python fire_mpi_simulate.py 10 0.1 20
 
 cd ~
-rm -rf spmd ~/hostfile ~/.openmpi/mca-params.conf stest
+rm -rf spmd ~/hostfile ~/.openmpi/mca-params.conf ctest
 
