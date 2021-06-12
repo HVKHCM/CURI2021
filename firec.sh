00644ae01b78eccfab98ea35c17caf3225398ba9
Dick Brown <rab@stolaf.edu>
2021-06-12 14:43:26 -0500

    Fix firec.sh - create fix.sh to be executed by pi

--- firec.sh
+++ firec.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # test fire on cluster and cleanup after cluster-set test
-# to execute:
+# to execute:  hd-cluster should execute
 # git clone https://github.com/HVKHCM/CURI2021-Raspberry-Pi.git ctest; ctest/firec.sh
 
 # test fire with cluster
 echo "Testing fire example with cluster"
 cd ~/CSinParallel/Exemplars/mpi4py-examples/fire/
-cp ~/ctest/fire_mpi_simulate.py .
 mpirun -np 4 -hostfile ~/hostfile python fire_mpi_simulate.py 10 0.1 20
 
 cd ~
-rm -rf spmd ~/hostfile ~/.openmpi/mca-params.conf ctest
+rm -rf spmd ~/hostfile ~/.openmpi/mca-params.conf ctest stest
 
